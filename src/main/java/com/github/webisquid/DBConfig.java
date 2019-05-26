4078448cdc08785c15627bd23fa48a13d08677ba
bakteriums <e.glushenko@hotmail.com>
2019-05-26 13:00:12 +0300

    add using db prop

--- src/main/java/com/github/webisquid/DBConfig.java
+++ src/main/java/com/github/webisquid/DBConfig.java
@@ -1,4 +1,29 @@
 package com.github.webisquid;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DBConfig {
+
+    Properties properties = new Properties();
+
+    FileInputStream fis;
+
+    {
+        try {
+            fis = new FileInputStream("src/main/java/resources/application.properties");
+            properties.load(fis);
+            String uri = properties.getProperty("db.host");
+            String login = properties.getProperty("db.user");
+            String pssword = properties.getProperty("db.login");
+            String port = properties.getProperty("db.port");
+            String DBname = properties.getProperty("db.name");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
